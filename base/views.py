1e6ddf456eba278fccdc665e8984f51963de3b6e
Igor Phllipe <igorphellipe.123@gmail.com>
2023-08-08 14:37:47 -0300

    Atualização Pet-Online
    Atualização do Código deixando com uma base fixa e a reutilizando nas demais páginas acrescidas.

--- base/views.py
+++ base/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from base.forms  import ContatoForm
 #Definindo a View - Função 
 def inicio(request):
     return render(request, 'inicio.html')
 
 def contato(request):
+    sucesso = False
+    if request.method == 'GET':
+        form = ContatoForm()
+    else:
+        form = ContatoForm(request.POST)
+        if form.is_valid():
+            sucesso = True
     contexto = {
         'telefone': '(99) 99999-9999',
-        'responsavel': 'Igor phellipe da silva passos'
+        'responsavel': 'Igor Phellipe',
+        'form': form,
+        'sucesso': sucesso
     }
-    if request.method == 'POST':
-        #Submissão de dados enviados pelo formulário, e mostrar como print o que foi enviado
-        print(request.POST)
-        print(request.POST['Nome'])
-        print(request.POST['E-mail'])
-        print(request.POST['Mensagem'])
     return render(request, 'contato.html', contexto)
