8d01c7073ad654c8736ee9b4c2224e83fc912b34
Igor Phllipe <igorphellipe.123@gmail.com>
2023-08-18 16:04:39 -0300

    Atualização - Projeto Pet-Online

--- reserva/forms.py
+++ reserva/forms.py
@@ -7,12 +7,12 @@ class ReservaForm(forms.ModelForm):
     def clean_data(self):
         data = self.cleaned_data['data']
         hoje = date.today()
-        reservas = Reserva.objects.filter(data=data).count()
-        if data < hoje:
+        dat = Reserva.objects.filter(data=data).count()
+        if dat > 4:
+            raise forms.ValidationError('Não é possivel realizar reserva')
+        elif data < hoje:
             raise forms.ValidationError('Não é possivel realizar uma reserva para o passado')
-        elif reservas > 4:
-            raise forms.ValidationError('Não foi possivel fazer reserva para essa data!')
-        return reservas
+        return data 
 
 
     class Meta:
